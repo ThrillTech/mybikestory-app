0cea2ea7566aa82ca5f02f6680a52c557c861a21
ThrillTech <ben@thrilltech.co.za>
2026-04-13 13:33:50 +0200

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://mybikestory.co.za", lastModified: new Date() },
+    { url: "https://mybikestory.co.za/listings", lastModified: new Date() },
+    { url: "https://mybikestory.co.za/sell", lastModified: new Date() },
+    { url: "https://mybikestory.co.za/auth/sign-up", lastModified: new Date() },
+  ];
+}
