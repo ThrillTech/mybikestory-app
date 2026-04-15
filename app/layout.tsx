3a53817c0faec16edf661a98a050981c24bdb22a
ThrillTech <ben@thrilltech.co.za>
2026-04-15 11:10:40 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,15 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "https://www.mybikestory.co.za",
   },
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
   robots: {
     index: true,
     follow: true,
