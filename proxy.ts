cc1b94006a13c702260f09a8f266127a84c73760
ThrillTech <ben@thrilltech.co.za>
2026-04-30 18:05:06 +0200

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,8 @@
 import { updateSession } from "@/lib/supabase/proxy";
 import { type NextRequest } from "next/server";
-
 export async function proxy(request: NextRequest) {
   return await updateSession(request);
 }
-
 export const config = {
   matcher: [
     /*
@@ -12,9 +10,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - site.webmanifest (PWA manifest)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|site\\.webmanifest|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
